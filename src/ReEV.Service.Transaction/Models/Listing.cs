54fcf89ac2260794dfed7d2d4eaf894001669231
Pham Quoc Quyen <quyenpq.work@gmail.com>
2025-10-28 18:28:39 +0700

    init model payment service

---
+++ src/ReEV.Service.Transaction/Models/Listing.cs
@@ -0,0 +1,25 @@
+﻿using ReEV.Common;
+
+namespace ReEV.Service.Transaction.Models
+{
+    public sealed class Listing : Entity
+    {
+        public string Title { get; set; }
+        public string MainImage { get; set; }
+        public Guid SellerId { get; set; }
+        public User Seller { get; set; } = null!;
+
+        private Listing(Guid listingId, string title, string mainImage, Guid sellerId)
+        {
+            Id = listingId;
+            Title = title;
+            MainImage = mainImage;
+            SellerId = sellerId;
+        }
+
+        public static Listing Create(Guid listingId, string title, string mainImage, Guid sellerId)
+        {
+            return new Listing(listingId, title, mainImage, sellerId);
+        }
+    }
+}
---
+++ src/ReEV.Service.Transaction/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using ReEV.Common;
+
+namespace ReEV.Service.Transaction.Models
+{
+    public sealed class User : Entity
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        private User(Guid id, string fullName, string phoneNumber, string email)
+        {
+            Id = id;
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public static User Create(Guid id, string fullName, string phoneNumber, string email)
+        {
+            return new User(id, fullName, phoneNumber, email);
+        }
+    }
+}
